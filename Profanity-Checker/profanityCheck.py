a34f8c1fe75eb30940590df23c7908a388119543
William Griffin <40611326+evelandy@users.noreply.github.com>
2018-08-06 01:21:45 -0500

    Profanity-Checker
    This quick app will take user input as filepath-filename.ext and will let the user know if the selected file has profanity in it

---
+++ Profanity-Checker/profanityCheck.py
@@ -0,0 +1,36 @@
+"""evelandy/W.G.
+Aug. 06 2018 10:38pm
+Profanity Checker
+Python36-32
+to use, just type the extention and filename.ext
+of the file you want to check when prompted. """
+#!/usr/bin/python
+
+import urllib.request
+from urllib.parse import urlencode
+
+
+filename = input("Enter the filename and extention of your file")
+
+def read_txt():
+    with open(filename) as file:
+        doc = file.read()
+    print(doc)
+    profan_chk(doc)
+
+
+def profan_chk(txt):
+    rule = urlencode({' ' : txt})
+    with urllib.request.urlopen("http://wdylike.appspot.com/?q="+rule) as site:
+        results = site.read()
+    print(results)
+
+    if results == b'false':
+        print("all good here!")
+    else:
+        print("check your text!!!")
+
+
+
+read_txt()
+
